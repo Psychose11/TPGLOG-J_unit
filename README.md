7665b242dbd8d92532a70db00414b9d5863e684a
psychose <fetrasarobidy3@gmail.com>
2024-03-31 12:14:06 +0200

    surcharge de la méthode equals class:Money /test réussi

---
+++ README.md
@@ -0,0 +1,6 @@
+# Test unitaire J_unit 4 
+
+## TP GLOG 😎
+
+![Capture d'écran 1](test_réussi.png)
+
--- src/MoneyTest/Money.java
+++ src/MoneyTest/Money.java
@@ -1,26 +1,35 @@
 package MoneyTest;
 
 public class Money {
+    private int fAmount;
+    private String fCurrency;
 
-	
-	
-	private int fAmount;
-	private String fCurrency;
-	public Money(int amount, String currency) {
-	fAmount = amount;
-	fCurrency = currency;
-	}
-	public int amount() {
-	return fAmount;
-	}
-	public String currency() {
-	return fCurrency;
-	}
-	public Money add(Money m) {
-	return new Money(amount() + m.amount(), currency());
-	}
+    public Money(int amount, String currency) {
+        fAmount = amount;
+        fCurrency = currency;
+    }
 
-	
-	
-	
+    public int amount() {
+        return fAmount;
+    }
+
+    public String currency() {
+        return fCurrency;
+    }
+
+    public Money add(Money m) {
+        return new Money(amount() + m.amount(), currency());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Money other = (Money) obj;
+        return fAmount == other.fAmount && fCurrency.equals(other.fCurrency);
+    }
 }
