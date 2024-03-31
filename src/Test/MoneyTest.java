300ebd3f18dd96d2f1f22457cc454794906e71c2
psychose <fetrasarobidy3@gmail.com>
2024-03-31 11:58:52 +0200

    creation class:Money et MoneyTest ajout de Junit4 /commit initial

---
+++ src/Test/MoneyTest.java
@@ -0,0 +1,21 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import MoneyTest.Money;
+
+public class MoneyTest {
+
+	@Test
+	public void testSimpleAdd() {
+	Money m12CHF = new Money(12, "CHF"); // création de données
+	Money m14CHF = new Money(14, "CHF");
+	Money expected = new Money(26, "CHF");
+	Money result = m12CHF.add(m14CHF); // exécution de la méthode testée
+	assertTrue(expected.equals(result)); // comparaison
+	}
+
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TPGLOG {
+	requires junit;
+}
